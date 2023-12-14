d5b75b130dbf349b49b5f9fd3f0f130d3b724423
Randy Woolford <rando@Randys-MBP.ht.home>
2023-12-14 13:06:49 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='highlevel saas oriented executive dashboard',
+    author='rando',
+    license='MIT',
+)
